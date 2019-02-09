92a5230d06b410fedfe26e23c7ff8aaea2706edd
Elephant_Man <grogan.charlie@gmail.com>
2019-02-08 22:32:25 -0600

    TDD counter

--- create-react-app/src/components/App.js
+++ create-react-app/src/components/App.js
@@ -1,13 +1,24 @@
 import React, { Component } from "react";
-import Wallet from "./Wallet";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      counter: 0
+    };
+  }
+
   render() {
     return (
-      <div>
-        <h2>Loot Check</h2>
-        <hr />
-        <Wallet />
+      <div data-test="component-app">
+        <h1 data-test="counter-display">{this.state.counter}</h1>
+        <button
+          data-test="increment-button"
+          onClick={() => this.setState({ counter: this.state.counter + 1 })}
+        >
+          Incement counter
+        </button>
       </div>
     );
   }
