b5041586fb0a59acc3c0c90c31c4e3812e4a655c
Elephant_Man <grogan.charlie@gmail.com>
2019-02-11 00:12:29 -0600

    CommentBox testing complete

--- boilerPlate/src/components/CommentBox.js
+++ boilerPlate/src/components/CommentBox.js
@@ -1,6 +1,28 @@
 import React, { Component } from "react";
 
-const CommentBox = () => {
-  return <div>Comment Box</div>;
-};
+class CommentBox extends Component {
+  state = { comment: "" };
+
+  handleChange = event => {
+    this.setState({ comment: event.target.value });
+    // setState() is async!!!! re-renders when it wants - not immediately update component
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.setState({ comment: "" });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h4>Add a Comment</h4>
+        <textarea value={this.state.comment} onChange={this.handleChange} />
+        <div>
+          <button>Submit Comment</button>
+        </div>
+      </form>
+    );
+  }
+}
 export default CommentBox;
